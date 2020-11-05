a14f3f89c5fe5868463c2fa15d3e4b13d32d067c
stackoverjoe <46171703+stackoverjoe@users.noreply.github.com>
2020-11-04 17:22:52 -0800

    Restructure client side web app, create a starting place login form - need to update colors and logos with project assets.

--- client/app.js
+++ client/app.js
@@ -3,6 +3,7 @@ var app = express();
 
 //Ejs for possible templating/server side rendering
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", function(req, res) {
   res.render("pages/login");
---
+++ client/public/js/login.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+//Login code will go here
+
+
+})
