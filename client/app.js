ec3f03c64fa1d678b6650757454df6f0fe0a3674
stackoverjoe <46171703+stackoverjoe@users.noreply.github.com>
2020-11-10 16:20:37 -0800

    Added fields to the map screen to reflect the mockups, addressed css issue on firefox, added support for twilio and sending text messages to users.

--- client/app.js
+++ client/app.js
@@ -1,5 +1,11 @@
+const path = require('path')
+require('dotenv').config({path: path.resolve(__dirname, '.env')});
 var express = require('express');
 var app = express();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
 
 //Ejs for possible templating/server side rendering
 app.set("view engine", "ejs");
@@ -15,6 +21,18 @@ app.get("/report", function(req, res) {
 
 app.get("/map", function(req, res) {
   res.render("pages/map");
+})
+
+app.get("/message", function(req, res) {
+  //twilio sms message example, sends me (joe) a text message
+  client.messages
+  .create({
+     body: 'Hey we can send people sms messages!',
+     from: '+13158185951',
+     to: '+19713376569'
+   })
+  .then(message => console.log(message.sid));
+  res.render("pages/login");
 })
 
 app.listen(8080);
