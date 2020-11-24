011ae299725d02ceed7738de9e46af92430903d3
stackoverjoe <46171703+stackoverjoe@users.noreply.github.com>
2020-11-24 09:36:20 -0800

    Start creating a mock json array that represents a possible return value from an API, todo: use this fake data to generate the reports in the templates. Swap this out for api data when applicable.

--- client/app.js
+++ client/app.js
@@ -1,5 +1,5 @@
 const path = require('path')
-require('dotenv').config({path: path.resolve(__dirname, '.env')});
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 var express = require('express');
 var app = express();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
@@ -8,26 +8,140 @@ const client = require('twilio')(accountSid, authToken);
 const date = require('date-and-time')
 const bcrypt = require('bcrypt')
 
+var mockData = [
+  {
+    id: "192571027510",
+    firstName: "Dan",
+    lastName: "Ryan",
+    isClockedIn: false,
+    currentLocation: {
+      longitude: 124.1515,
+      latitude: 241.10581
+    },
+    jobs: [
+      {
+        id: "13001755210489",
+        name: "The gig 1",
+        pay: 24.25,
+        location: {
+          street: "1244 salmon st",
+          city: "Portland",
+          state: "OR",
+          zip: "97215",
+          longitude: 124.10555,
+          latitude: 241.020202
+        }
+      },
+      {
+        id: "1792571925712957",
+        name: "The gig 2",
+        pay: 24.25,
+        location: {
+          street: "1244 salmon st",
+          city: "Portland",
+          state: "OR",
+          zip: "97215",
+          longitude: 124.10555,
+          latitude: 241.020202
+        }
+      }
+    ],
+    payHistory: [
+      [
+        {
+          date: "11/8/2020",
+          job: "The gig 1",
+          in: "12:30 PM",
+          out: "8:30 PM",
+          reg: 8,
+          OT: 0,
+          Total: 8,
+          grossPay: 194.00
+        },
+        {
+          date: "11/9/2020",
+          job: "The gig 1",
+          in: "12:30 PM",
+          out: "8:30 PM",
+          reg: 8,
+          OT: 0,
+          Total: 8,
+          grossPay: 194.00
+        },
+        {
+          date: "11/10/2020",
+          job: "The gig 1",
+          in: "12:30 PM",
+          out: "8:30 PM",
+          reg: 8,
+          OT: 0,
+          Total: 8,
+          grossPay: 194.00
+        },
+        {
+          date: "11/11/2020",
+          job: "The gig 1",
+          in: "12:30 PM",
+          out: "8:30 PM",
+          reg: 8,
+          OT: 0,
+          Total: 8,
+          grossPay: 194.00
+        },
+        {
+          date: "11/12/2020",
+          job: "The gig 1",
+          in: "12:30 PM",
+          out: "8:30 PM",
+          reg: 8,
+          OT: 0,
+          Total: 8,
+          grossPay: 194.00
+        },
+        {
+          date: "11/13/2020",
+          job: "The gig 1",
+          in: "12:30 PM",
+          out: "8:30 PM",
+          reg: 8,
+          OT: 0,
+          Total: 8,
+          grossPay: 194.00
+        },
+        {
+          date: "11/14/2020",
+          job: "The gig 1",
+          in: "12:30 PM",
+          out: "8:30 PM",
+          reg: 8,
+          OT: 0,
+          Total: 8,
+          grossPay: 194.00
+        },
+      ]
+    ]
+  }
+]
 
 //Ejs for possible templating/server side rendering
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 app.use(require("body-parser").json());
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.render("pages/login");
 });
 
-app.get("/report", function(req, res) {
+app.get("/report", function (req, res) {
   res.render("pages/reportPage");
 })
 
-app.get("/map", function(req, res) {
+app.get("/map", function (req, res) {
   var currentTime = date.format(new Date(), 'hh:mm:ss A')
-  res.render("pages/map", {time: currentTime});
+  res.render("pages/map", { time: currentTime });
 })
 
-app.post("/login-check", function(req, res) {
+app.post("/login-check", function (req, res) {
   const username = req.body.username
   const password = req.body.password
   //This is where the database will be called and bcrypt compare issued to see if login was valid. Also will asign session token
@@ -35,15 +149,15 @@ app.post("/login-check", function(req, res) {
   res.json("200")
 })
 
-app.get("/message", function(req, res) {
+app.get("/message", function (req, res) {
   //twilio sms message example, sends me (joe) a text message
   client.messages
-  .create({
-     body: 'Hey we can send people sms messages!',
-     from: '+13158185951',
-     to: '+19713376569'
-   })
-  .then(message => console.log(message.sid));
+    .create({
+      body: 'Hey we can send people sms messages!',
+      from: '+13158185951',
+      to: '+19713376569'
+    })
+    .then(message => console.log(message.sid));
   res.render("pages/login");
 })
 
