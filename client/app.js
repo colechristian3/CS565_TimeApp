e7082808f47230f2af1dd0c69e64f5ba918ffc28
stackoverjoe <46171703+stackoverjoe@users.noreply.github.com>
2020-11-24 10:05:57 -0800

    update report generator to get data from mock data blob

--- client/app.js
+++ client/app.js
@@ -8,120 +8,126 @@ const client = require('twilio')(accountSid, authToken);
 const date = require('date-and-time')
 const bcrypt = require('bcrypt')
 
-var mockData = [
-  {
-    id: "192571027510",
-    firstName: "Dan",
-    lastName: "Ryan",
-    isClockedIn: false,
-    currentLocation: {
-      longitude: 124.1515,
-      latitude: 241.10581
+var mockData =
+{
+  id: "192571027510",
+  firstName: "Dan",
+  lastName: "Ryan",
+  isClockedIn: false,
+  currentLocation: {
+    longitude: 124.1515,
+    latitude: 241.10581
+  },
+  jobs: [
+    {
+      id: "13001755210489",
+      name: "The gig 1",
+      pay: 24.25,
+      location: {
+        street: "1244 salmon st",
+        city: "Portland",
+        state: "OR",
+        zip: "97215",
+        longitude: 124.10555,
+        latitude: 241.020202
+      }
     },
-    jobs: [
+    {
+      id: "1792571925712957",
+      name: "The gig 2",
+      pay: 24.25,
+      location: {
+        street: "1244 salmon st",
+        city: "Portland",
+        state: "OR",
+        zip: "97215",
+        longitude: 124.10555,
+        latitude: 241.020202
+      }
+    }
+  ],
+  payHistory: [
+    [
       {
-        id: "13001755210489",
-        name: "The gig 1",
-        pay: 24.25,
-        location: {
-          street: "1244 salmon st",
-          city: "Portland",
-          state: "OR",
-          zip: "97215",
-          longitude: 124.10555,
-          latitude: 241.020202
-        }
+        date: "11/8/2020",
+        job: "The gig 1",
+        in: "12:30 PM",
+        out: "8:30 PM",
+        reg: 8,
+        ot: 0,
+        total: 8,
+        grossPay: 194.00
       },
       {
-        id: "1792571925712957",
-        name: "The gig 2",
-        pay: 24.25,
-        location: {
-          street: "1244 salmon st",
-          city: "Portland",
-          state: "OR",
-          zip: "97215",
-          longitude: 124.10555,
-          latitude: 241.020202
-        }
+        date: "11/9/2020",
+        job: "The gig 1",
+        in: "12:30 PM",
+        out: "8:30 PM",
+        reg: 8,
+        ot: 0,
+        total: 8,
+        grossPay: 194.00
+      },
+      {
+        date: "11/10/2020",
+        job: "The gig 1",
+        in: "12:30 PM",
+        out: "8:30 PM",
+        reg: 8,
+        ot: 0,
+        total: 8,
+        grossPay: 194.00
+      },
+      {
+        date: "11/11/2020",
+        job: "The gig 1",
+        in: "12:30 PM",
+        out: "8:30 PM",
+        reg: 8,
+        ot: 0,
+        total: 8,
+        grossPay: 194.00
+      },
+      {
+        date: "11/12/2020",
+        job: "The gig 1",
+        in: "12:30 PM",
+        out: "8:30 PM",
+        reg: 8,
+        ot: 0,
+        total: 8,
+        grossPay: 194.00
+      },
+      {
+        date: "11/13/2020",
+        job: "The gig 1",
+        in: "12:30 PM",
+        out: "8:30 PM",
+        reg: 8,
+        ot: 0,
+        total: 8,
+        grossPay: 194.00
+      },
+      {
+        date: "11/14/2020",
+        job: "The gig 1",
+        in: "12:30 PM",
+        out: "8:30 PM",
+        reg: 8,
+        ot: 0,
+        total: 8,
+        grossPay: 194.00
+      },
+      totals = {
+        totalPay: "1359.00",
+        totalHours: 56,
+        totalOT: 0,
+        totalCombined: 56
       }
-    ],
-    payHistory: [
-      [
-        {
-          date: "11/8/2020",
-          job: "The gig 1",
-          in: "12:30 PM",
-          out: "8:30 PM",
-          reg: 8,
-          OT: 0,
-          Total: 8,
-          grossPay: 194.00
-        },
-        {
-          date: "11/9/2020",
-          job: "The gig 1",
-          in: "12:30 PM",
-          out: "8:30 PM",
-          reg: 8,
-          OT: 0,
-          Total: 8,
-          grossPay: 194.00
-        },
-        {
-          date: "11/10/2020",
-          job: "The gig 1",
-          in: "12:30 PM",
-          out: "8:30 PM",
-          reg: 8,
-          OT: 0,
-          Total: 8,
-          grossPay: 194.00
-        },
-        {
-          date: "11/11/2020",
-          job: "The gig 1",
-          in: "12:30 PM",
-          out: "8:30 PM",
-          reg: 8,
-          OT: 0,
-          Total: 8,
-          grossPay: 194.00
-        },
-        {
-          date: "11/12/2020",
-          job: "The gig 1",
-          in: "12:30 PM",
-          out: "8:30 PM",
-          reg: 8,
-          OT: 0,
-          Total: 8,
-          grossPay: 194.00
-        },
-        {
-          date: "11/13/2020",
-          job: "The gig 1",
-          in: "12:30 PM",
-          out: "8:30 PM",
-          reg: 8,
-          OT: 0,
-          Total: 8,
-          grossPay: 194.00
-        },
-        {
-          date: "11/14/2020",
-          job: "The gig 1",
-          in: "12:30 PM",
-          out: "8:30 PM",
-          reg: 8,
-          OT: 0,
-          Total: 8,
-          grossPay: 194.00
-        },
-      ]
     ]
-  }
-]
+  ]
+}
+
 
 //Ejs for possible templating/server side rendering
 app.set("view engine", "ejs");
@@ -133,7 +139,7 @@ app.get("/", function (req, res) {
 });
 
 app.get("/report", function (req, res) {
-  res.render("pages/reportPage");
+  res.render("pages/reportPage", { data: mockData });
 })
 
 app.get("/map", function (req, res) {
