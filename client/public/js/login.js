8a12c2687df54e6f70ce0faea0a0e81859eefcd7
stackoverjoe <46171703+stackoverjoe@users.noreply.github.com>
2020-11-27 10:36:07 -0800

    Add tooltips to require username and password when logging in.

--- client/public/js/login.js
+++ client/public/js/login.js
@@ -1,32 +1,54 @@
-$(document).ready(function(){
-$("#loginButton").click(function(){
-  const username = $("#username").val()
-  const password = $("#password").val()
+$(document).ready(function () {
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip({ trigger: "manual" })
+  })
 
-  fetch("/login-check", {
-  method: "post",
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  //make sure to serialize your JSON body
-  body: JSON.stringify({
-    "username": `${username}`,
-    "password": `${password}`
+  $('input').focus(function () {
+    $(".tooltip").tooltip("hide")
   })
+
+  $("#loginButton").click(function () {
+    const username = $("#username").val()
+    const password = $("#password").val()
+
+    if (!username && !password) {
+      $("#username").tooltip('show')
+      $("#password").tooltip('show')
+    }
+    else if (!username) {
+      $("#username").tooltip('show')
+    }
+    else if (!password) {
+      $("#password").tooltip('show')
+    }
+    else {
+      fetch("/login-check", {
+        method: "post",
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        //make sure to serialize your JSON body
+        body: JSON.stringify({
+          "username": `${username}`,
+          "password": `${password}`
+        })
+      })
+        .then((response) => {
+          return response.json()
+
+        }).then(response => {
+          if (response === "200") {
+            //set a token
+            //validate toke on all subsequent screens
+            window.location.href = '/report'
+          }
+          else {
+            $("#badAttempt").text("Invalid login credentials.")
+            //bad login attemp
+          }
+        });
+    }
   })
-  .then( (response) => {
-  return response.json()
-   //do something awesome that makes the world a better place
-  }).then(response => {
-  if(response === "200"){
-    //set a token
-    //validate toke on all subsequent screens
-    window.location.href='/report'
-  }
-  else {
-    //bad login attemp
-  }
-  });
-})
+
 
 })
--- client/public/js/report.js
+++ client/public/js/report.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
-  //Login code will go here
   $('#excelExport').click(function () {
-
     $('.reportTable').tableExport({ type: 'excel' });
   })
 })
